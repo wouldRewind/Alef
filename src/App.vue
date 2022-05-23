1ca918969bffec26e0c5c4fe0a99b0935ef0dc87
Nick Suhanov <nik.152002@mail.ru>
2022-05-23 15:57:39 +0300

    c1: place project structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/form">Form</router-link> |
+    <router-link to="/preview">Preview</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/PreviewView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">Preview</div>
+</template>
+
+<script>
+export default {
+  name: "PreviewView",
+  components: {},
+};
+</script>
