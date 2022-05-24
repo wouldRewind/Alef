4a656fdf89c0fc79bc412ab631d04d529d363381
Nick Suhanov <nik.152002@mail.ru>
2022-05-24 17:38:14 +0300

    c-12: complete save button design

---
+++ src/components/Form/Button/Save.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="wrapper">
+		<button
+		@click="$emit('onSave')" 
+		type="button" 
+		class="button">
+			Сохранить
+		</button>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/scss/_const';
+	.wrapper {
+		display: inline-block;
+	}
+	.button {
+		font-family: "Montserrat", serif;
+		border: none;
+		cursor: pointer;
+		transition: all .3s ease;
+		background: $blueColor;
+		color: white;
+		padding: 10px 20px;
+		font-size: 14px;
+		border-radius: 100px;
+		&:hover {
+			background: lighten($blueColor, 5);
+		}
+	}
+</style>
