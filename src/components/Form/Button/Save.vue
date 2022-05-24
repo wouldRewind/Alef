84da2b25a4713ad654e13d4f65b64cbdd4311201
Nick Suhanov <nik.152002@mail.ru>
2022-05-24 23:41:58 +0300

    c-15: add disable state

--- src/components/Form/Button/Save.vue
+++ src/components/Form/Button/Save.vue
@@ -3,11 +3,23 @@
 		<button
 		@click="$emit('onSave')" 
 		type="button" 
-		class="button">
+		:class="['button', {'button_disabled': disabled }]">
 			Сохранить
 		</button>
 	</div>
 </template>
+
+
+<script>
+export default {
+	props: {
+		disabled: {
+			type: Boolean,
+			default: false,
+		}
+	},
+}
+</script>
 
 <style lang="scss" scoped>
 @import '@/scss/_const';
@@ -18,14 +30,23 @@
 		font-family: "Montserrat", serif;
 		border: none;
 		cursor: pointer;
-		transition: all .3s ease;
+		transition: $transition;
 		background: $blueColor;
 		color: white;
 		padding: 10px 20px;
 		font-size: 14px;
 		border-radius: 100px;
-		&:hover {
-			background: lighten($blueColor, 5);
+		border: 1px solid transparent;
+		&:hover:not(&_disabled) {
+			background: white;
+			color: $blueColor;
+			border: 1px solid $blueColor;
+		}
+		&_disabled {
+			background: transparent;
+			color: $disableColor;
+			border: 1px solid $disableColor;
+			cursor: default;
 		}
 	}
 </style>
