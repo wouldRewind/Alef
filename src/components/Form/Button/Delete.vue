77e3e338cd0331c427f1d9c9a20c53861854a6f4
Nick Suhanov <nik.152002@mail.ru>
2022-05-24 17:38:45 +0300

    c-12: complete delete button design

---
+++ src/components/Form/Button/Delete.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="wrapper">
+		<button
+		@click="onDelete"
+		class="delete"
+		type="button">
+			Удалить
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'DeleteButton',
+	props: {
+		onDelete: {
+			type: Function,
+			required: false,
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/_const';
+	.wrapper {
+		display: inline-block;
+	}
+	.delete {
+		border: none;
+		background: transparent;
+		cursor: pointer;
+		transition: $transition;
+		color: $blueColor;
+		font-size: 14px;
+		&:hover {
+			color: darken($blueColor, 10);
+		}
+	}
+</style>
+
